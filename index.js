c5b3358add7b7b19e06d3db51bf6f4a5c881c3f6
José Luis Lafuente <jl@lafuente.me>
2019-04-16 00:23:29 +0200

    Use conjure RPC server

--- index.js
+++ index.js
@@ -1,15 +1,55 @@
 const { sources, workspace } = require("coc.nvim");
+const { Socket } = require("net");
+
+
+function sendMessage(client, message) {
+  return new Promise((resolve, reject) => {
+    client.write(message);
+
+    client.on("data", data => {
+      resolve(data);
+    });
+
+    client.on("error", err => {
+      reject(err);
+    });
+  });
+}
+
 
 exports.activate = async context => {
   const { nvim } = workspace;
+  let client = null;
+
+  async function newClient() {
+    const client = new Socket();
+    const port = await nvim.eval("conjure#get_rpc_port()");
+    client.connect(port, "127.0.0.1");
+    return client;
+  }
+
+  async function conjureCompletions(input) {
+    if (client == null) {
+      client = await newClient();
+    }
+    // See
+    // https://github.com/Olical/conjure/blob/master/rplugin/python3/deoplete/sources/conjure.py
+    const req = [0, 1, "completions", [input]];
+
+    const response = await sendMessage(client, JSON.stringify(req));
+    return JSON.parse(response)[3];
+  }
 
   context.subscriptions.push(
     sources.createSource({
       name: "conjure",
       triggerCharacters: [],
       doComplete: async function(opt) {
-        if (!opt.input) return null;
-        const res = await nvim.eval(`conjure#completions("${opt.input}")`);
+        const input = opt.input;
+        if (!input) return null;
+
+        const res = await conjureCompletions(input);
+
         if (!res || res.length === 0) return null;
         return { items: res };
       }
