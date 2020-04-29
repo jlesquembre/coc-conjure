e34413be524a38a589a510b0d3eb6cd1195cb97a
Oliver Caldwell <ollie@oli.me.uk>
2020-04-29 14:33:54 +0100

    Make completions truly async
    Relying on node's event loop instead of blocking Neovim until there's a
    result.

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 const { sources, workspace } = require("coc.nvim");
 const { nvim } = workspace;
 
+async function lua(mod, f, arg) {
+  return nvim.lua("return require('" + mod + "')['" + f + "'](...)", arg)
+}
+
+async function snooze(ms) {
+  return new Promise((res) => {
+    setTimeout(res, ms)
+  })
+}
+
 async function completions(input) {
-  return nvim.lua("return require('conjure.eval')['completions-sync'](...)", input)
+  const p = await lua("conjure.eval", "completions-promise", input)
+
+  while (!await lua("conjure.promise", "done?", p)) {
+    await snooze(20)
+  }
+
+  return lua("conjure.promise", "close", p)
 }
 
 exports.activate = async context => {
