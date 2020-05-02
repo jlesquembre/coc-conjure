e72acb785e483805d690b918efb48bf8ba570a6a
José Luis Lafuente <jl@lafuente.me>
2020-05-02 18:46:50 +0200

    Prettier

--- index.js
+++ index.js
@@ -2,37 +2,37 @@ const { sources, workspace } = require("coc.nvim");
 const { nvim } = workspace;
 
 async function lua(mod, f, arg) {
-  return nvim.lua("return require('" + mod + "')['" + f + "'](...)", arg)
+  return nvim.lua("return require('" + mod + "')['" + f + "'](...)", arg);
 }
 
 async function snooze(ms) {
   return new Promise((res) => {
-    setTimeout(res, ms)
-  })
+    setTimeout(res, ms);
+  });
 }
 
 async function completions(input) {
-  const p = await lua("conjure.eval", "completions-promise", input)
+  const p = await lua("conjure.eval", "completions-promise", input);
 
-  while (!await lua("conjure.promise", "done?", p)) {
-    await snooze(20)
+  while (!(await lua("conjure.promise", "done?", p))) {
+    await snooze(20);
   }
 
-  return lua("conjure.promise", "close", p)
+  return lua("conjure.promise", "close", p);
 }
 
-exports.activate = async context => {
+exports.activate = async (context) => {
   context.subscriptions.push(
     sources.createSource({
       name: "conjure",
       triggerCharacters: [".", "/", ":"],
-      doComplete: async function(opt) {
+      doComplete: async function (opt) {
         const input = opt.input;
         if (!input) return null;
         const res = await completions(input);
         if (!res || res.length === 0) return null;
         return { items: res };
-      }
+      },
     })
   );
 };
